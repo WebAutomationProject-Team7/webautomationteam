bd071133dbfa0aa5763524d58eb918f13321032a
Mridue <arif.mridue@gmail.com>
2019-01-06 21:38:38 -0500

    "Adjustment of master branch"

--- Generic/src/main/java/base/CommonApi.java
+++ Generic/src/main/java/base/CommonApi.java
@@ -10,19 +10,18 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class CommonApi {
-
     public static WebDriver driver = null;
     public static WebDriverWait wait = null;
 
     @Parameters({/*"useCloudEnv","cloudEnvName", */"os", "os_version", "browserName", "browserVersion", "url"})
     @BeforeMethod
     public void setUp(/*@Optional("false") boolean useCloudEnv, @Optional("false") String cloudEnvName,*/
-            @Optional("windows") String os, @Optional("10") String os_version, @Optional("firefox") String browserName, @Optional("34")
-            String browserVersion, @Optional("www.google.com") String url) throws IOException {
+                     @Optional("windows") String os, @Optional("10") String os_version, @Optional("firefox") String browserName, @Optional("34")
+                     String browserVersion, @Optional("www.google.com") String url) throws IOException {
         getLocalDriver(browserName, os);
-        wait = new WebDriverWait(driver, 40);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(55, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         driver.get(url);
         driver.manage().window().maximize();
     }
@@ -30,8 +29,7 @@ public class CommonApi {
     public WebDriver getLocalDriver(String browserName, String os) {
         if (browserName.equalsIgnoreCase("chrome")) {
             if (os.equalsIgnoreCase("windows")) {
-
-              
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\arifq\\eclipse-workspace\\webautomationpractice\\Generic\\Drivers\\chromedriver.exe");
                 driver = new ChromeDriver();
             } else if (os.equalsIgnoreCase("mac")) {
                 System.setProperty("webdriver.chrome.driver", "../Generic/drivers/mac/chromedriver");
@@ -39,21 +37,24 @@ public class CommonApi {
             }
         } else if (browserName.equalsIgnoreCase("firefox")) {
             if (os.equalsIgnoreCase("windows")) {
-                System.setProperty("webdriver.gecko.driver", "/Users/salmaalam/IdeaProjects/webautomationteam/Generic/MacDriver/geckodrivertionpractice\\Generic\\Drivers\\geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\arifq\\eclipse-workspace\\webautomationpractice\\Generic\\Drivers\\geckodriver.exe");
                 driver = new FirefoxDriver();
             } else if (os.equalsIgnoreCase("mac")) {
-                System.setProperty("webdriver.gecko.driver", "/Users/salmaalam/IdeaProjects/webautomationteam/Generic/MacDriver/geckodriver");
+                System.setProperty("webdriver.gecko.driver", "../Generic/drivers/mac/geckodriver");
                 driver = new FirefoxDriver();
             }
         }
         return driver;
     }
+
+
     @AfterMethod
     public void tearDown() {
         // driver.close();
         driver.quit();
     }
+
+
+   
+
 }
-
-
-
