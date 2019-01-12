092111fa3494b4749aef6b0491e9fce76ed9c27a
salmaalam <salmaalam12122004@gmail.com>
2019-01-12 03:59:11 -0500

    test cases added

--- Cigna/src/main/java/pageNobject/OwnPage.java
+++ Cigna/src/main/java/pageNobject/OwnPage.java
@@ -1,6 +1,7 @@
 package pageNobject;
 
 import base.CommonApi;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,30 +12,50 @@ import org.testng.annotations.Test;
 public class OwnPage extends CommonApi {
 
 
-    public OwnPage(){
-        PageFactory.initElements (driver,this);
+    public OwnPage() {
+        PageFactory.initElements(driver, this);
     }
-    @FindBy(css = "#includes-content > div.d-none.d-lg-block > nav:nth-child(2) > div > a > svg")
-    WebElement logo;
 
+    @FindBy(css = "#includes-content > div.d-none.d-lg-block > nav:nth-child(2) > div > a > svg")
+    public WebElement logo;
     @FindBy(xpath = "//div[@id=\"includes-content\"]/div/nav[2]/div/ul/li[2]/a")
     public WebElement loginPage;
     @FindBy(xpath = "//div[@id='includes-content']/div[1]/nav[1]/div/ul/li[2]/a")
-    public WebElement  contactUs;
+    public WebElement contactUs;
     @FindBy(css = "#includes-content > div.d-none.d-lg-block > nav.navbar.global-nav.universal-navigation.navbar-expand-lg.navbar-white.bg-white.pt-lg-0 > div > ul > li:nth-child(1) > a")
     public WebElement cignaInternationnal;
-    @FindBy(xpath = "//*[@id='oo_no_thanks']")//*[@id="oo_no_thanks"]
-    WebElement noThanks;
     @FindBy(css = "#includes-content > div.d-none.d-lg-block > nav:nth-child(2) > div > ul > li:nth-child(2) > a")
-    WebElement logintomyCigna;
-    @FindBy(xpath = "//div[@id='includes-content']/div[1]/nav[3]/div/ul/li[1]/a" )
-    WebElement individualFamilies;
+    public WebElement logintomyCigna;
+    @FindBy(xpath = "//div[@id='includes-content']/div[1]/nav[3]/div/ul/li[1]/a")
+    public WebElement individualFamilies;
     @FindBy(xpath = "//div[@id=\"includes-content\"]/div/nav[3]/div/ul/li[2]/a")
-    WebElement employersNbrokers;
+    public WebElement employersNbrokers;
     @FindBy(xpath = "//div[@id='includes-content']/div/nav[3]/div/ul/li[3]/a")
-    WebElement healthCareProviders;
+    public WebElement healthCareProviders;
     @FindBy(xpath = "//div[@id=\"includes-content\"]/div/nav[2]/div/ul/li[1]/a")
-    WebElement findDoctors;
+    public WebElement findDoctors;
+    @FindBy(name = "query")
+    public WebElement searchButton;
+    @FindBy(css = "a[class='link-action mb-m link-external']")
+    public WebElement shopMedicalPlanes;
+    @FindBy(xpath = "//div[@id='1524743177700']/div/div/div[2]/div/p[3]/a")
+    public WebElement shopDentalPlans;
+    @FindBy(xpath = "//div[@id=\"1524743178566\"]/div/div[1]/div/div/div[1]/div[2]/a")
+    public WebElement healthInsuranceTab;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[1]/div/div/div[2]/div[2]/a")
+    public WebElement medicareProducts;
+    @FindBy(xpath = "//div[@id=\"1524743178566\"]/div/div[1]/div/div/div[3]/div[2]/a")
+    public WebElement medicareSupplementInsurance;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[1]/div/div/div[4]/div[2]/a")
+    public WebElement dentalInsurance;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[1]/div/div/div[5]/div[2]/a")
+    public WebElement otherInsurance;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[1]/div/div/div[6]/div[2]/a")
+    public WebElement internationalCoverage;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[3]/div/div/div[1]/div[2]/h4/a")
+    public WebElement employeePlanNPolicy;
+    @FindBy(xpath = "//div[@id='1524743178566']/div/div[3]/div/div/div[2]/div[2]/h4/a")
+    public WebElement employerGroupSolutions;
 
 
     public String checkContactUs() {
@@ -43,34 +64,38 @@ public class OwnPage extends CommonApi {
         System.out.println(url);
         return url;
     }
+
     public String cignaInternational() {
         cignaInternationnal.click();
         String url = driver.getCurrentUrl();
         System.out.println(url);
         return url;
     }
-    public boolean logoDisplayed(){
+
+    public boolean logoDisplayed() {
         boolean logoDisplayed = logo.isDisplayed();
         return logoDisplayed;
     }
+
     public String loginUrl() throws InterruptedException {
-        logintomyCigna.click ();
-        Thread.sleep (2000);
-        System.out.println (driver.getTitle ());
-        for(String loginTab:driver.getWindowHandles ()){
-            driver.switchTo ().window (loginTab);
+        logintomyCigna.click();
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        for (String loginTab : driver.getWindowHandles()) {
+            driver.switchTo().window(loginTab);
         }
-        System.out.println (driver.getTitle ());
-        String url = driver.getCurrentUrl ();
+        System.out.println(driver.getTitle());
+        String url = driver.getCurrentUrl();
         return url;
     }
-    public String contactUs(){
+
+    public String contactUs() {
         String text = contactUs.getText();
         System.out.println(contactUs.getText());
         return text;
     }
 
-    public String checkIndividualFamilies(){
+    public String checkIndividualFamilies() {
         individualFamilies.click();
         String url = driver.getCurrentUrl();
         System.out.println(url);
@@ -78,7 +103,7 @@ public class OwnPage extends CommonApi {
     }
 
 
-    public String checkEmployerNbrokers(){
+    public String checkEmployerNbrokers() {
         employersNbrokers.click();
         String url = driver.getCurrentUrl();
         System.out.println(url);
@@ -86,14 +111,14 @@ public class OwnPage extends CommonApi {
     }
 
 
-    public String checkHealthCareProvider(){
+    public String checkHealthCareProvider() {
         healthCareProviders.click();
         String url = driver.getCurrentUrl();
         System.out.println(url);
         return url;
     }
 
-    public String checkFindDoctors(){
+    public String checkFindDoctors() {
         findDoctors.click();
         String url = driver.getCurrentUrl();
         System.out.println(url);
@@ -101,19 +126,104 @@ public class OwnPage extends CommonApi {
 
     }
 
-    public String checkLoginPage(){
+    public String checkLoginPage() {
         loginPage.click();
         String url = driver.getCurrentUrl();
-        System.out.println();
         System.out.println(url);
         return url;
 
     }
 
+    public String checkSearchBox() {
+        searchButton.sendKeys("Medical Claim Form");
+        searchButton.submit();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
 
 
+    public String checkMedicalPlanBox() {
+        shopMedicalPlanes.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
 
+
+    public String checkShopDentalPlans() {
+        shopDentalPlans.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
+
+    public String checkHealthInsuranceTab() {
+        healthInsuranceTab.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+
+
+    }
+
+    public String checkMedicareProducts() {
+        medicareProducts.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
+
+
+    public String checkMedicareSupplementInsurance() {
+        medicareSupplementInsurance.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
+
+    public String checkDentalInsurance() {
+        dentalInsurance.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+
+
+    }
+
+    public String checkOtherInsurance() {
+        otherInsurance.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+
+
+    }
+
+    public String checkInternationalCoverage() {
+        internationalCoverage.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+
+    }
+
+    public String checkEmployeePlanNPolicy() {
+        employeePlanNPolicy.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+
+    }
+
+    public String checkEmployerGroupSolutions() {
+        employerGroupSolutions.click();
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        return url;
+    }
 
 }
+
 
 
--- Cigna/src/test/java/TestCase/TestOwnPage.java
+++ Cigna/src/test/java/TestCase/TestOwnPage.java
@@ -15,75 +15,166 @@ public class TestOwnPage extends CommonApi {
 
     @BeforeMethod
     public void initilizationOfObject() {
-       ownPage = PageFactory.initElements(driver,OwnPage.class);
-       signInPage = PageFactory.initElements(driver, SignInPage.class);
+        ownPage = PageFactory.initElements(driver, OwnPage.class);
+        signInPage = PageFactory.initElements(driver, SignInPage.class);
     }
 
-
-     @Test
-   public void contactUsButton(){
-        String actual= ownPage.checkContactUs();
-       String expect= "https://www.cigna.com/about-us/contact-us/";
-         Assert.assertEquals(actual,expect);
-     }
 
     @Test
-    public void cignaInternational(){
+    public void contactUsButton() {
+        String actual = ownPage.checkContactUs();
+        String expect = "https://www.cigna.com/about-us/contact-us/";
+        Assert.assertEquals(actual, expect);
+    }
+
+    @Test
+    public void cignaInternational() {
         String actual = ownPage.cignaInternational();
         String expect = "https://www.cigna.com/international/";
-        Assert.assertEquals(actual,expect);
+        Assert.assertEquals(actual, expect);
     }
-        @Test
-        public void logoDisplayed(){
+
+    @Test
+    public void logoDisplayed() {
         boolean actual = ownPage.logoDisplayed();
         Assert.assertTrue(actual);
     }
+
     @Test
     public void yTubeLinkTest() throws InterruptedException {
         String actual = ownPage.loginUrl();
         String expected = "https://my.cigna.com/web/public/guest";
-        Assert.assertEquals (actual,expected);
+        Assert.assertEquals(actual, expected);
     }
+
     @Test
-    public void contactUs(){
+    public void contactUs() {
         String actual = ownPage.contactUs();
         String expect = "Contact Us";
-        Assert.assertEquals(actual,expect);
+        Assert.assertEquals(actual, expect);
 
     }
 
     @Test
 
-    public void checkIndividualFamilies(){
-        String actual=ownPage.checkIndividualFamilies();
-        String expect ="https://www.cigna.com/individuals-families/";
-        Assert.assertEquals(actual,expect);
+    public void checkIndividualFamilies() {
+        String actual = ownPage.checkIndividualFamilies();
+        String expect = "https://www.cigna.com/individuals-families/";
+        Assert.assertEquals(actual, expect);
     }
 
     @Test
-    public void checkEmployeeNbroker(){
+    public void checkEmployeeNbroker() {
         String actual = ownPage.checkEmployerNbrokers();
         String expect = "https://www.cigna.com/employers-brokers/";
-        Assert.assertEquals(actual,expect);
+        Assert.assertEquals(actual, expect);
     }
 
     @Test
-    public void checkHealthCareProviders(){
+    public void checkHealthCareProviders() {
         String actual = ownPage.checkHealthCareProvider();
         String expect = "https://www.cigna.com/health-care-providers/";
-        Assert.assertEquals(actual,expect);
+        Assert.assertEquals(actual, expect);
     }
 
     @Test
-    public void checkFindDoctors(){
+    public void checkFindDoctors() {
         String actual = ownPage.checkFindDoctors();
         String expect = "https://www.cigna.com/hcpdirectory/";
-        Assert.assertEquals(actual,expect);
+        Assert.assertEquals(actual, expect);
     }
 
 
+    @Test
+
+    public void checkSearchButton() {
+        String actual = ownPage.checkSearchBox();
+        String expect = " https://www.cigna.com/search?query=medical%20claim%20form";
+
+    }
 
 
+    @Test
+    public void checkShopMedicalBox() {
+        String actual = ownPage.checkMedicalPlanBox();
+        String expect = "https://www.cigna.com/medicare/supplemental/?campaign_ID=CSBORG";
+
+
+    }
+
+    @Test
+    public void checkDenatalPlan() {
+        String actual = ownPage.checkShopDentalPlans();
+        String expect = "https://www.cigna.com/individuals-families/plans-services/dental-insurance-plans/";
+        Assert.assertEquals(actual, expect);
+    }
+
+    @Test
+    public void checkHealthInsuranceTab() {
+        String actual = ownPage.checkHealthInsuranceTab();
+        String expect = "https://www.cigna.com/individuals-families/plans-services/health-insurance-plans/";
+        Assert.assertEquals(actual, expect);
+
+    }
+
+    @Test
+
+    public void checkMedicareProducts() {
+        String actual = ownPage.checkMedicareProducts();
+        String expect = "https://www.cigna.com/medicare/";
+
+
+    }
+
+    @Test
+    public void checkMedicareSupplementInsurance() {
+        String actual = ownPage.checkMedicareSupplementInsurance();
+        String expect = "https://www.cigna.com/medicare/supplemental/?campaign_ID=CSBORG";
+
+
+    }
+
+    @Test
+    public void checkDentalInsurance() {
+        String actual = ownPage.checkDentalInsurance();
+        String expect = "https://www.cigna.com/individuals-families/plans-services/dental-insurance-plans/";
+        Assert.assertEquals(actual, expect);
+    }
+
+
+    @Test
+    public void checkOtherInsurance() {
+        String actual = ownPage.checkOtherInsurance();
+        String expect = "https://www.cigna.com/individuals-families/plans-services/supplemental/";
+        Assert.assertEquals(actual, expect);
+
+
+    }
+
+    @Test
+    public void checkInternationalCoverage() {
+        String actual = ownPage.checkInternationalCoverage();
+        String expect = "https://www.cigna.com/international/";
+        Assert.assertEquals(actual, expect);
+    }
+
+    @Test
+    public void checkEmployeePlanNPolicy() {
+        String actual = ownPage.checkEmployeePlanNPolicy();
+        String expect = "https://www.cigna.com/individuals-families/plans-services/plans-through-employer/#filter=plantype/medical-plans/";
+
+
+    }
+
+    @Test
+    public void checkEmployerGroupSolutios() {
+        String actual = ownPage.checkEmployerGroupSolutions();
+        String expect = "https://www.cigna.com/employers-brokers/plans-services/#filter=plantype/medical-plans/group/all-group-sizes";
+        Assert.assertEquals(actual, expect);
+
+    }
 
 }
+
+
 
