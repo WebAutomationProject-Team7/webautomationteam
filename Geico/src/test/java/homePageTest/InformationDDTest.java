2a3a9ff4ec3b163d751f3090835961ebcaa542f8
Mridue <arif.mridue@gmail.com>
2019-01-11 21:44:00 -0500

    pom issue solved for arif

---
+++ Geico/src/test/java/homePageTest/InformationDDTest.java
@@ -0,0 +1,20 @@
+package homePageTest;
+
+import homePage.InformationDD;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class InformationDDTest extends InformationDD {
+    InformationDD informationDD;
+    @BeforeMethod
+    public void init(){
+        informationDD = PageFactory.initElements(driver, InformationDD.class);
+    }
+    @Test
+    public void testInformationMenuItems() {
+        String s = String.join(", ",getInformationMenuList());
+        Assert.assertEquals("My Account, Claims and Incidents, Tools and Resources, About GEICO, Web and Mobile, Contact Us", s);
+    }
+}
