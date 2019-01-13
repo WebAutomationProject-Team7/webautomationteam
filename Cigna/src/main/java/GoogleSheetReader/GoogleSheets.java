c2f030d1811ceee1aa3ee42bb1d019cff3f890ae
salmaalam <salmaalam12122004@gmail.com>
2019-01-13 14:37:11 -0500

    changes

--- Cigna/src/main/java/GoogleSheetReader/GoogleSheets.java
+++ Cigna/src/main/java/GoogleSheetReader/GoogleSheets.java
@@ -1,32 +1,10 @@
 package GoogleSheetReader;
 
-import base.CommonApi;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.ValueRange;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static googlesheetapi.GoogleSheetReader.getSheetsService;
+public class GoogleSheets  {
 
-public class GoogleSheets extends CommonApi {
-
-    public List<List<Object>> getSpreadSheetRecords(String spreadsheetId, String range) throws IOException {
-        Sheets service = getSheetsService();
-        ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
-        List<List<Object>> values = response.getValues();
-        if (values == null || values.size() == 0) {
-            return null;
-        } else {
-            return values;
-        }
-    }
-
-    }
-
+}
 
 
 
