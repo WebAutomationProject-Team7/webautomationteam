730d43cdf9b125247d88c77b1024a347fc626a1d
Zaya0619 <zaykamnts@gmail.com>
2019-01-19 09:53:06 -0500

    AmazonDotCom

---
+++ Amazon/src/main/java/excelReader/LoginPage.java
@@ -0,0 +1,36 @@
+package excelReader;
+
+import base.CommonApi;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import reporting.TestLogger;
+
+public class LoginPage extends CommonApi {
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//*[@id='nav-link-accountList']/span[1]")
+    public static WebElement signIn;
+    @FindBy(xpath = "//*[@id='ap_email']")
+    public static WebElement user_ID;
+    @FindBy(xpath = "//*[@id='ap_password']")
+    public static WebElement password;
+    @FindBy(xpath = "//*[@id='signInSubmit']")
+    public static WebElement signInBox;
+    @FindBy(xpath = "//*[@id='auth-error-message-box']/div/h4")
+    public static WebElement loginErrorMessage;
+
+    public String userLogin(String userIDKey, String passwordKey) throws InterruptedException {
+
+//        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+//        }.getClass().getEnclosingMethod().getName()));
+        signIn.click();
+        user_ID.sendKeys(userIDKey);
+        password.sendKeys(passwordKey);
+        signIn.click();
+        return loginErrorMessage.getText();
+    }
+
+}
